40d89cb3ddaf74cf83ef81a756a69598792838bc
Inzamul Hoque <inzamol@gmail.com>
2024-12-16 23:11:38 +0530

    cleaned

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -23,21 +23,7 @@ from rest_framework_simplejwt.views import (
     TokenBlacklistView
 )
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
-# from drf_yasg.views import get_schema_view
-# from drf_yasg import openapi
 
-# schema_view = get_schema_view(
-#     openapi.Info(
-#         title="Your API Title",
-#         default_version='v1',
-#         description="API documentation for your project",
-#         terms_of_service="https://www.google.com/policies/terms/",
-#         contact=openapi.Contact(email="your_email@example.com"),
-#         license=openapi.License(name="BSD License"),
-#     ),
-#     public=True,
-#     permission_classes=(permissions.AllowAny,),
-# )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -55,6 +41,4 @@ urlpatterns = [
     # Optional UI:
     path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-    # path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
